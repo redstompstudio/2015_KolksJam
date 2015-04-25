7d7557d8ecf653a49a5ad17700dd4a01398278b0
lucasanizelli <lucasanizelli@gmail.com>
2015-04-25 14:21:59 -0300

    Objects LeanTween

---
+++ UnityProject/KolksJam/Assets/Scripts/UISplash.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UISplash : MonoBehaviour
+{
+    void Start()
+    {
+        RectTransform ImageRectTransform = gameObject.GetComponent<RectTransform>();
+        SpriteRenderer Image = gameObject.GetComponent<SpriteRenderer>();
+
+
+        ImageRectTransform.sizeDelta = new Vector2(Screen.width, Screen.height);
+    }
+}
