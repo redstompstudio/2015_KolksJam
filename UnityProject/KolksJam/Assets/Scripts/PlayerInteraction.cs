4af95d6c1df3209b08041f48bc57e61323c8ab51
Arthur Fukushima <arthuryoshinori@gmail.com>
2015-04-25 11:35:21 -0300

    yrlyrk

---
+++ UnityProject/KolksJam/Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInteraction : MonoBehaviour
+{
+	public Transform playerHeadRef;
+	public float interactionDistance = 1.0f;
+
+	private const string interactableTagName = "Interactable";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.E))
+		{
+			RaycastHit hit;
+
+			if(Physics.Raycast(playerHeadRef.position, playerHeadRef.forward, out hit, interactionDistance))
+			{
+				if(hit.transform.CompareTag(interactableTagName))
+				{
+					hit.transform.SendMessage("ExecuteAction", SendMessageOptions.DontRequireReceiver);
+				}
+			}
+		}
+	}
+}
