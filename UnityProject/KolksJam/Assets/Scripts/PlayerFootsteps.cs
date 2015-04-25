5cfc7a668164cea7e1c5d9b47554e6dfe712add4
Arthur Fukushima <arthuryoshinori@gmail.com>
2015-04-25 13:07:35 -0300

    - Footstep Sounds. - Breathing Sound.

---
+++ UnityProject/KolksJam/Assets/Scripts/PlayerFootsteps.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerFootsteps : MonoBehaviour 
+{
+	private Rigidbody cachedRigidbody;
+
+	void Awake()
+	{
+		cachedRigidbody = GetComponent<Rigidbody>();
+	}
+
+	public void Update()
+	{
+
+	}
+}
---
+++ UnityProject/KolksJam/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class PlayerScript : MonoBehaviour
+{
+	public Transform playerHeadRef;
+	public float interactionDistance = 1.0f;
+
+	private const string interactableTagName = "Interactable";
+
+	public Light flashLight;
+
+	public bool isStressed;
+	public AudioSource stressSound;
+
+	public bool playFootstepSounds;
+	public AudioSource footStepSound;
+
+	private RigidbodyFirstPersonController controller;
+
+	void Awake()
+	{
+		controller = GetComponent<RigidbodyFirstPersonController>();
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.F))
+		{
+			flashLight.enabled = !flashLight.enabled;
+		}
+
+		if(Input.GetKeyDown(KeyCode.E))
+		{
+			RaycastHit hit;
+
+			if(Physics.Raycast(playerHeadRef.position, playerHeadRef.forward, out hit, interactionDistance))
+			{
+				if(hit.transform.CompareTag(interactableTagName))
+					hit.transform.SendMessage("ExecuteAction", SendMessageOptions.DontRequireReceiver);
+			}
+		}
+
+		StressSound();
+		FootStepSounds();
+	}
+
+	public void StressSound()
+	{
+		if(stressSound != null)
+		{
+			if(isStressed && !stressSound.isPlaying)
+			{
+				stressSound.Play();
+			}
+			else if(!isStressed && stressSound.isPlaying)
+			{
+				stressSound.Stop();
+			}
+		}
+	}
+
+
+	public void FootStepSounds()
+	{
+		if(!playFootstepSounds || footStepSound == null)
+			return;
+
+		if(controller.Grounded && controller.Velocity.sqrMagnitude > 1.0f)
+		{
+			if(!footStepSound.isPlaying)
+				footStepSound.Play();
+		}
+		else if(!controller.Grounded || controller.Velocity.sqrMagnitude <= 1.0f)
+		{
+			if(footStepSound.isPlaying)
+				footStepSound.Stop();
+		}
+	}
+}
