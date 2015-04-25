fc41d3b6cb18c2f7571f4065ea7b5d9ed95abee6
lucasanizelli <lucasanizelli@gmail.com>
2015-04-25 15:24:31 -0300

    Splash Update

--- UnityProject/KolksJam/Assets/Scripts/UISplash.cs
+++ UnityProject/KolksJam/Assets/Scripts/UISplash.cs
@@ -5,33 +5,45 @@ using UnityEngine.UI;
 public class UISplash : MonoBehaviour
 {
     Image ImageRef;
+    public CanvasGroup Splash;
+
+    bool fadeIn = false;
+    bool fadeOut = false;
 
     void Start()
     {
         RectTransform ImageRectTransform = gameObject.GetComponent<RectTransform>();
         ImageRef = gameObject.GetComponent<Image>();
 
-        ImageRef.color = new Color(1f, 1f, 1f, 0f);
+        Splash.alpha = 0f;
         ImageRectTransform.sizeDelta = new Vector2(Screen.width, Screen.height);
 
         StartCoroutine("AnimSplash");
 
     }
 
+    void Update()
+    {
+        if (fadeIn && Splash.alpha < 1)
+            Splash.alpha += 2f * Time.deltaTime;
+
+        if (fadeOut && Splash.alpha > 0)
+            Splash.alpha -= 2f * Time.deltaTime;
+    }
+
     IEnumerator AnimSplash()
     {
         yield return new WaitForSeconds(1f);
-        LeanTween.value(gameObject, UpdateAlpha, 0f, 1f, 1.5f);
+        fadeIn = true;
         yield return new WaitForSeconds(3.5f);
-        LeanTween.value(gameObject, UpdateAlpha, 1f, 0f, 1.5f);
-        yield return new WaitForSeconds(2.5f);
+        fadeIn = false;
+        fadeOut = true;
+        yield return new WaitForSeconds(1.5f);
+        StarGame();
+    }
+
+    void StarGame()
+    {
         Application.LoadLevel("Player_Movement");
     }
-
-    void UpdateAlpha(float f)
-    {
-        ImageRef.color = new Color(1f, 1f, 1f, f);
-    }
-
-    
 }
