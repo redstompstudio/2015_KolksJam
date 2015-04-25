9a3221aad38078715907d9f7b7d0a7b57ce71b73
Arthur Fukushima <arthuryoshinori@gmail.com>
2015-04-25 16:02:10 -0300

    - Player - Pescoçada (lean: Q e E)

---
+++ UnityProject/KolksJam/Assets/Scripts/Player/PlayerHead.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHead : MonoBehaviour 
+{
+	public Transform leanLeftRef;
+	public Transform leanRightRef;
+	private Vector3 originalPosition;
+
+	// Use this for initialization
+	void Start () 
+	{
+		originalPosition = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKey(KeyCode.Q))	
+		{
+			transform.localPosition = Vector3.Lerp(transform.localPosition, leanLeftRef.localPosition, Time.deltaTime * 5.0f);
+		}
+		else if(Input.GetKey(KeyCode.E))
+		{
+			transform.localPosition = Vector3.Lerp(transform.localPosition, leanRightRef.localPosition, Time.deltaTime * 5.0f);
+		}
+		else
+		{
+			transform.localPosition = Vector3.Lerp(transform.localPosition, originalPosition, Time.deltaTime * 5.0f);
+		}
+	}
+}
