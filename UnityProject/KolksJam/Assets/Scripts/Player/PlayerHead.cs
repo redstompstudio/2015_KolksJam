fecadd73efc6c3987d13d17e303706d03854d137
Arthur Fukushima <arthuryoshinori@gmail.com>
2015-04-26 17:07:41 -0300

    - Player Bob Head...

--- UnityProject/KolksJam/Assets/Scripts/Player/PlayerHead.cs
+++ UnityProject/KolksJam/Assets/Scripts/Player/PlayerHead.cs
@@ -18,11 +18,19 @@ public class PlayerHead : MonoBehaviour
 	{
 		if(Input.GetKey(KeyCode.Q))	
 		{
-			transform.localPosition = Vector3.Lerp(transform.localPosition, leanLeftRef.localPosition, Time.deltaTime * 5.0f);
+			if(!Physics.Raycast(transform.position, -transform.right, 0.5f))
+				transform.localPosition = Vector3.Lerp(transform.localPosition, leanLeftRef.localPosition, Time.deltaTime * 5.0f);
+			else
+				transform.localPosition = Vector3.Lerp(transform.localPosition, originalPosition, Time.deltaTime * 5.0f);
 		}
 		else if(Input.GetKey(KeyCode.E))
 		{
 			transform.localPosition = Vector3.Lerp(transform.localPosition, leanRightRef.localPosition, Time.deltaTime * 5.0f);
+
+			if(!Physics.Raycast(transform.position, -transform.right, 0.5f))
+				transform.localPosition = Vector3.Lerp(transform.localPosition, leanRightRef.localPosition, Time.deltaTime * 5.0f);
+			else
+				transform.localPosition = Vector3.Lerp(transform.localPosition, originalPosition, Time.deltaTime * 5.0f);
 		}
 		else
 		{
