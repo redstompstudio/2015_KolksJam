18610f15b2c8f9ff66efe2d2bf9f4478f66c1ce9
Arthur Fukushima <arthuryoshinori@gmail.com>
2015-04-26 20:23:30 -0300

    - Cutscene Final

---
+++ UnityProject/KolksJam/Assets/Scripts/FinalCutscene.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinalCutscene : MonoBehaviour 
+{
+	public Camera cutsceneCamera;
+	public GameObject crosshairRef;
+
+	public AudioSource deathSFX;
+
+	public GameObject morenaDaCutscene;
+
+	public Transform valveCamRef;
+	public Light valveSpotlight;
+	public AudioSource tamtaaaaam;
+
+	public void StartCutscene()
+	{
+		Camera.main.gameObject.SetActive(false);
+		cutsceneCamera.enabled = true;
+		crosshairRef.SetActive(false);
+
+		deathSFX.Play();
+		morenaDaCutscene.SetActive(true);
+
+		StartCoroutine(EmposoredByValve());
+	}
+
+	void OnGUI()
+	{
+		if(GUILayout.Button("Start Cutscene"))
+		{
+			StartCutscene();
+		}
+	}
+
+	IEnumerator EmposoredByValve()
+	{
+		yield return new WaitForSeconds(4.0f);
+		valveSpotlight.enabled = true;
+
+		cutsceneCamera.transform.position = valveCamRef.position;
+		cutsceneCamera.transform.rotation = valveCamRef.rotation;
+
+		tamtaaaaam.Play();
+	}
+}
+	
