7d34fc9545076b8be214e71f44f838cd028d27d2
Arthur Fukushima <arthuryoshinori@gmail.com>
2015-04-26 15:48:29 -0300

    - Portas - Cena com hélices...

---
+++ UnityProject/KolksJam/Assets/Scripts/Objects/RotateAround.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateAround : MonoBehaviour 
+{
+	public float rotationSpeed = 10.0f;
+	public Vector3 axis = Vector3.up;
+
+	private Transform cachedTransform;
+
+	void Awake()
+	{
+		cachedTransform = transform;
+	}
+
+	void Update()
+	{
+		cachedTransform.RotateAround(axis, rotationSpeed * Time.deltaTime);
+	}
+}
